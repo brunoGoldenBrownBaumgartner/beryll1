c519f82f492a34dad0d70a6509aaa1f3380e67fa
brunoGoldenBrownBaumgartner <164511778+brunoGoldenBrownBaumgartner@users.noreply.github.com>
2024-03-25 22:47:40 +0100

    Add files via upload
    update

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeartCanvas from './heart/Heart';
+import SwipeMenu from './menu/menu';
+
+const App = () => {
+  return (
+    <div>
+      <HeartCanvas />
+      <SwipeMenu />
+    </div>
+  );
+};
+
+export default App;
